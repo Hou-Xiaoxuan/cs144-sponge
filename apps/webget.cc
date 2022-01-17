2204582d0423e702699f3f0d5412a9ac883a9b03
LinXuan <hou_xiaoxuan@qq.com>
2022-01-18 00:56:02 +0800

    通过webget check

--- apps/webget.cc
+++ apps/webget.cc
@@ -16,9 +16,29 @@ void get_URL(const string &host, const string &path) {
     // Then you'll need to print out everything the server sends back,
     // (not just one call to read() -- everything) until you reach
     // the "eof" (end of file).
+    // cout<<"开始"<<endl;
 
-    cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
-    cerr << "Warning: get_URL() has not been implemented yet.\n";
+    TCPSocket tcp_socket;
+    // 客户端使用connect建立连接
+    tcp_socket.connect(Address(host, "http"));
+    // 发送get请求
+    tcp_socket.write("GET " + path + " HTTP/1.1\r\n"); 
+    // host part
+    tcp_socket.write("Host: " + host + "\r\n"); 
+    // 告诉服务器，返回response后即可断开连接
+    tcp_socket.write("Connection: close\r\n");
+    tcp_socket.write("\r\n");
+
+    // shutdown
+    tcp_socket.shutdown(SHUT_WR);
+    // cout<<"收到回复："<<endl;
+    while(tcp_socket.eof() == false){
+        string recived = tcp_socket.read();
+        cout<<recived;
+    }
+    tcp_socket.close();
+    // cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
+    // cerr << "Warning: get_URL() has not been implemented yet.\n";
 }
 
 int main(int argc, char *argv[]) {
