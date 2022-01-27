e2ef6ba1e34a0e5b41bd719d06d6a4b4b51b456d
LinXuan <hou_xiaoxuan@qq.com>
2022-01-27 23:30:29 +0800

    通过lab1测试     总时间花费5h30m。其中,3h用于阅读要求并构思实现方法,其余时间用于code和debug.     但是,在编码时,仍有很多关于程序预期行为的错误理解,以及编写时的细小逻辑错误.     1. _buffer变量名打错(挨打)     2. 没有检查assembed部分的数据重复,造成size_t移除     3. 通过比对测试样例,发现自己理解错了capacity的定义.正确的定义更自然     希望下一次能认真理解题意,并做出好的设计

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -1,5 +1,5 @@
 #include "stream_reassembler.hh"
-
+# include<iostream>
 // Dummy implementation of a stream reassembler.
 
 // For Lab 1, please replace with a real implementation that passes the
@@ -12,15 +12,100 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity) {}
+StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity){}
 
+void StreamReassembler::_make_output()
+{
+    // ！考虑异常安全性
+    string to_write("");
+    for(size_t i = 0; i<this->_buffer.size(); i++)
+    {
+        if(this->_vis[i])
+        {
+            to_write.push_back(this->_buffer[i]);
+        }
+        else
+        {
+            break;
+        }
+    }
+    size_t written = this->_output.write(to_write);
+
+    // 仅删除确保写入的部分
+    while(written--)
+    {
+        this->_buffer.pop_front();
+        this->_vis.pop_front();
+        this->_ack++;
+        this->_unassembled_bytes--;
+    }
+
+    // 检查是否eof
+    if(this->_ack >= this->_eof)
+    {
+        // 停止写入
+        this->_output.end_input();
+    }
+}
+
+size_t StreamReassembler::_extend_buffer(size_t target)
+{
+
+    while(target > this->_buffer.size() and this->_exist_capacity()<this->_capacity)
+    {
+        this->_buffer.push_back(' ');
+        this->_vis.push_back(false);
+    }
+    return this->_buffer.size();
+}
+
+size_t StreamReassembler::_exist_capacity()
+{
+    return this->_output.buffer_size() + this->_buffer.size();
+}
 //! \details This function accepts a substring (aka a segment) of bytes,
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
-void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    DUMMY_CODE(data, index, eof);
+void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof)
+{
+    if(eof == true)
+    {
+        // 到达末尾标记结尾
+        // 是否已经写入到_output由_make_output()判断
+        this->_eof = index + data.size();
+    }
+
+    // 检查数据是否已经排序，从data的start位置开始插入。
+    size_t start = 0ul;
+    if(index < this->_ack)
+    {
+        if(index + data.size() < this->_ack){
+            start = data.size();
+        }
+        else{
+            start = this->_ack - index;
+        }
+    }
+    size_t offset = index - this->_ack;
+
+    size_t cap = this->_extend_buffer(offset + data.size());
+
+    for(size_t i = start; i<data.size() and i+offset < cap; i++)
+    {
+        
+        // 放入数据
+        this->_buffer[offset + i] = data[i];
+        if(this->_vis[offset + i] == false)
+        {
+            // 标记染色
+            this->_vis[offset + i] = true;
+            // 增加记录
+            this->_unassembled_bytes++;
+        }
+    }
+    this->_make_output();
 }
 
-size_t StreamReassembler::unassembled_bytes() const { return {}; }
+size_t StreamReassembler::unassembled_bytes() const { return this->_unassembled_bytes; }
 
-bool StreamReassembler::empty() const { return {}; }
+bool StreamReassembler::empty() const { return this->_unassembled_bytes == 0ul; }
--- libsponge/stream_reassembler.hh
+++ libsponge/stream_reassembler.hh
@@ -5,7 +5,27 @@
 
 #include <cstdint>
 #include <string>
-
+#include <deque>
+/*
+* 参考一些中文的翻译和解释，任务大致如下：
+- StreamRessembler被用在TCPReviver中，将乱序、会重复的报文段重组，并放入ByteStream中
+- 已经排好序的报文需要放入ByteStream中。同时缓存未排好序的报文
+- ~~capacity的容量是未排好序的缓存中的加上已经排好序、放入ByteStream中大小~~
+- error: capacity理解有误。乱序中，未收到的部分也计入capacity。
+  因此，capacity为_buffer和_output的容量和
+* 缓存的实现思路：
+    吐槽：要是规定滑动窗口的大小就可以不用考虑这个问题了
+    1.  用list<char>实现，乱序插入时，list中间填空缺。
+        但是，空缺的标记是一个问题。有两种思路：
+        - 用list<bool>标记，消耗双倍空间
+        - 用heap<pair<int, int>>储存，最坏情况消耗sizeof(int)*2+1倍空间
+        另外，插入中间片段的时间是O(n)
+    2.  用deque实现。不需要考虑中间存储的问题，但是消耗更多的空间，同时也有标记的问题。
+* duplicated.与overlap: 忽略已经确认过的，其余不做检查，直接重复复制、标记。
+    * error: 需要检查index是否小于_ack, 此时会造成offset过大，可能越界
+* 用_ack标记期待的下一个index。结合deque的索引访问数据
+* 注意_output也有容量，可以写入失败
+*/
 //! \brief A class that assembles a series of excerpts from a byte stream (possibly out of order,
 //! possibly overlapping) into an in-order byte stream.
 class StreamReassembler {
@@ -14,7 +34,23 @@ class StreamReassembler {
 
     ByteStream _output;  //!< The reassembled in-order byte stream
     size_t _capacity;    //!< The maximum number of bytes
+    std::deque<char> _buffer = std::deque<char>();      // 储存缓存的数据
+    std::deque<bool> _vis = std::deque<bool>();         // 标记存储过的数据
+    size_t _ack = 0;                                    // 期待的下一个标号
+    size_t _unassembled_bytes = 0;                      // 未排好序的数据
+    size_t _eof = size_t(-1);                           // 最后一字节的标号
 
+    /*私有接口*/
+    // 检查并将有序数据写入_output
+    void _make_output();
+
+    // 扩充_buffer以储存新的substring。
+    // 检查容量，扩充至容忍的最大
+    // target   目标大小
+    size_t _extend_buffer(size_t target);
+
+    // 返回已经占据的容量
+    size_t _exist_capacity();
   public:
     //! \brief Construct a `StreamReassembler` that will store up to `capacity` bytes.
     //! \note This capacity limits both the bytes that have been reassembled,
