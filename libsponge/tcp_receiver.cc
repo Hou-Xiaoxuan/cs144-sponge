c525a7651ede82ae353fc8701f2da4a9fbeaab3f
LinXuan <hou_xiaoxuan@qq.com>
2022-02-04 00:13:53 +0800

    通过lab2测试
    花费时间: 6~8小时不确定了,分多个阶段完成
    wrap-unwrap:
        不熟悉无符号数的使用,以及边界条件的设定,大量时间花在根据调试情况去修改分类情况上.
    ackno:
        没搞懂定位,
        具体来说,我保存的值,需要返回的值,使用接口需要的值分别对应seq,ab-swq,index中的哪一个不是很明了
        而我在stream_reasserbler中已经保存了一个私有的与ackno意义接近的值.因此选择改动旧代码,增加一个接口.
    window_size:
        搞错一次window_size的定义.需要依赖调试情况
    receive:
        花费时间反而较少,需要做的事情清晰明了.
    总结:
        我的初步设计还是存在很多不合理的地方,在后续过程中不断修改头文件.希望下一次我能有清晰完善的设计,从而更从容的进行实现编写:

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -10,26 +10,56 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
+void TCPReceiver::_set_syn(const TCPSegment &seg)
+{
+    // 设置initial segment number
+    // sequence number start from the isn
+    // isn is the first seq-no, set now
+    // ack is the next byte wanted--now, is the first seq-no
+    if(seg.header().syn){
+        this->_isn = seg.header().seqno;
+        this->_syn = true;
+    }
+}
+
 void TCPReceiver::segment_received(const TCPSegment &seg)
 {
-    if(this->_sin == false){
-        this->_set_sin(seg);
+    if(this->_syn == false){
+        this->_set_syn(seg);
     }
-    if(seg.header().fin == true){
-        this->_set_fin(seg);
-    }
-    if(seg.length_in_sequence_space() > 0ul){
-        this->_recieve_bytes(seg);
+
+    if(this->ackno().has_value())
+    {
+        // start receive bytes, calculate and change the ackno
+        // note: length_in_sequence_space()的接口已经累加了SIN和FIN的量
+        // note: 通过payload()返回read-only的buffer来获取数据
+        // note: 通过循环寻找ByteStream的byte_writen()来得到已经写入的byte数量——排好序的数量
+        auto buf = seg.payload();
+        bool flag = seg.header().fin;
+        // 从segno->absolutly-segno->stream_index
+        // 计算index时需要-1, 是SYN占据的量(FIN后不需要计算index了)
+        size_t checkpoint = this->stream_out().bytes_written();
+        size_t index = unwrap(seg.header().seqno, this->_isn, checkpoint);
+        if(seg.header().syn == false){
+            index -= 1ul; // 减去syn的量。第一个segment不考虑
+        }
+        this->_reassembler.push_substring(buf.copy(), index, flag);
     }
 }
 
 optional<WrappingInt32> TCPReceiver::ackno() const { 
-    if(this->_fin == false){
+    if(this->_syn == false){
         return optional<WrappingInt32>();
     }
-    return optional<WrappingInt32>(this->_ackno);
+    // 根据reseambled计算
+    size_t acki = this->_reassembler.ack();
+    if(true == this->stream_out().input_ended()){
+        return wrap(acki + 2, this->_isn);
+    } else{
+        return wrap(acki + 1, this->_isn);
+    }
 }
 
 size_t TCPReceiver::window_size() const { 
-    return this->_capacity - this->_reassembler.unassembled_bytes();
+    return this->_capacity - this->stream_out().buffer_size();
 }
